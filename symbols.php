eea38d8f4d8b39de64e49d6bb6c9a60ff9d60db1
Max Base <MaxBaseCode@Gmail.Com>
2021-01-05 20:29:51 +0330

    Delete symbols.php

--- symbols.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "base.php";
-
-$symbols = $db->selectsRaw("SELECT * FROM $db->db.`symbol` WHERE `rahavardID` IS NOT NULL AND `tsetmcID` IS NOT NULL AND `tsetmcINS` IS NOT NULL;");
-print_r($symbols);
-
